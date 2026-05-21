d7fde76a230ef6ae350ca1553100b1715414d0b5
Joaquin-Perez-Voges <49761366@est.ort.edu.ar>
2026-05-21 11:14:38 -0300

    Termine de hacer que el pendrive sea pickable

---
+++ Assets/Scipts/USBPickUp.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class USBPickUp : MonoBehaviour
+{
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.gameObject);
+        if(other.gameObject.CompareTag("Pickable"))
+        {
+            Destroy(other.gameObject);
+        }
+    }   
+}
